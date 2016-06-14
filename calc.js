6a9e097f1614088b640ba8d30708dca290564903
Stephanie <stephaniepui@outlook.com>
2016-06-13 22:16:37 -0700

    First attempt

--- calc.js
+++ calc.js
@@ -1,7 +1,22 @@
 #! /usr/bin/env node
 
 var userArgs = process.argv.slice(2);
-var numberInput = userArgs[0];
+var value1 = Number(userArgs[0]);
+var value2 = Number(userArgs[2]);
+
 var answer;
 
+if (userArgs.indexOf("+") >= 0) {
+  answer = value1 + value2;
+} else if (userArgs.indexOf("-") >= 0) {
+  answer = value1 - value2;
+} else if (userArgs.indexOf("x") >= 0) {
+  answer = value1 * value2;
+} else if (userArgs.indexOf("/") >= 0) {
+  answer = value1 / value2;
+} else if (userArgs.indexOf("%") >= 0) {
+  answer = value1 % value2;
+} else {answer = "I don't know how to do that!"}
+
+//console.log(answer);
 console.log(answer);
