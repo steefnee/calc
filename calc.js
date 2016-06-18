76e29ba315bab152bde84d1726fc0a1ce1a45858
Stephanie <stephaniepui@outlook.com>
2016-06-17 23:36:36 -0700

    first rework

--- calc.js
+++ calc.js
@@ -1,26 +1,17 @@
 #! /usr/bin/env node
-
+var answer;
 var userArgs = process.argv.slice(2);
+var mathThings = {"+": answer=value1 + value2, "-": answer=value1 - value2};
 var value1 = Number(userArgs[0]);
 var value2 = Number(userArgs[2]);
 
-var answer;
 
-if (userArgs.indexOf("+") >= 0) {
-  answer = value1 + value2;
-} else if (userArgs.indexOf("-") >= 0) {
-  answer = value1 - value2;
-} else if (userArgs.indexOf("x") >= 0) {
-  answer = value1 * value2;
-} else if (userArgs.indexOf("/") >= 0) {
-  answer = value1 / value2;
-} else if (userArgs.indexOf("%") >= 0) {
-  answer = value1 % value2;
-} else if (userArgs.indexOf("^") >=0) {
-  answer = Math.pow(value1, value2); 
-} else if (userArgs.indexOf("sqrt") >= 0) {
-  answer = Math.sqrt(value1);
-} else {answer = "I don't know how to do that!"}
 
+
+for (var key in mathThings) {
+  if (userArgs.indexOf(key) >= 0) {
+    answer=mathThings[key];
+  }
+}
 //console.log(answer);
 console.log(answer);
