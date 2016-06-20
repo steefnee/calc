b40ffbc07799a8da7c32d55915296e1a15c294c1
Stephanie <stephaniepui@outlook.com>
2016-06-19 19:33:16 -0700

    WIP

--- calc.js
+++ calc.js
@@ -1,24 +1,39 @@
 #! /usr/bin/env node
 
 var userArgs = process.argv.slice(2);
+var spl = problem.split(/[+x/%-]/);
+var result=spl.map(Number);
+
+var notInteger = function (result) {
+  for (var i = 0; i < result.length; i++) { 
+   if ( /^\d+$/.test(result[i]) === true)
+      {
+            return true;
+  }
+};
+
 var value1 = Number(userArgs[0]);
 var value2 = Number(userArgs[2]);
 
+
+
 var answer;
-
-if (userArgs.indexOf("+") >= 0) {
-  answer = value1 + value2;
-} else if (userArgs.indexOf("-") >= 0) {
-  answer = value1 - value2;
-} else if (userArgs.indexOf("x") >= 0) {
-  answer = value1 * value2;
-} else if (userArgs.indexOf("/") >= 0) {
-  answer = value1 / value2;
-} else if (userArgs.indexOf("%") >= 0) {
-  answer = value1 % value2;
-} else if (userArgs.indexOf("^") >=0) {
-  answer = Math.pow(value1, value2);
-} else {answer = "I don't know how to do that!"}
-
+if (notInteger(result) === true) {
+  console.log("Numbers only please");
+} else {
+    if (userArgs.indexOf("+") >= 0) {
+      answer = value1 + value2;
+    } else if (userArgs.indexOf("-") >= 0) {
+      answer = value1 - value2;
+    } else if (userArgs.indexOf("x") >= 0) {
+      answer = value1 * value2;
+    } else if (userArgs.indexOf("/") >= 0) {
+      answer = value1 / value2;
+    } else if (userArgs.indexOf("%") >= 0) {
+      answer = value1 % value2;
+    } else if (userArgs.indexOf("^") >=0) {
+      answer = Math.pow(value1, value2);
+    } else {answer = "I don't know how to do that!"}
+}
 //console.log(answer);
 console.log(answer);
