f8aa10ba8360779e75b4ca99fecde78bebff6056
Stephanie <stephaniepui@outlook.com>
2016-06-25 01:33:52 -0700

    Formatted to use in REPL

--- calc.js
+++ calc.js
@@ -1,45 +1,40 @@
-#! /usr/bin/env node
+var calc = function(userArgs) {
+  var originalProblem = userArgs.split('');
+  var result = userArgs.toString().split(/[+xps/%-]/).map(Number);
 
-var userArgs = process.argv.slice(2).join('');
-var result = userArgs.split(/[+xps/%-]/).map(Number);
+  var answer;
+  var value1 = result[0];
+  var value2 = result[1];
 
-var answer;
-var value1 = result[0];
-var value2 = result[1];
-
-var isInteger = function (result) {
-  for (var i = 0; i < result.length; i++) { 
-   if ( /^\d+$/.test(result[i]) === false)
-      {return false;}
-    else {return true;}
-  } 
-};
+  var isInteger = function (result) {
+    for (var i = 0; i < result.length; i++) { 
+     if ( /^\d+$/.test(result[i]) === false)
+        {return false;}
+      else {return true;}
+    } 
+  };
 
 
  if (isInteger(result) === false) {
    answer = "Numbers only please!";
  } else {
-
-    if (userArgs.indexOf("+") >= 0) {
+    if (originalProblem.indexOf("+") >= 0) {
       answer = value1 + value2;
-    } else if (userArgs.indexOf("-") >= 0) {
+    } else if (originalProblem.indexOf("-") >= 0) {
       answer = value1 - value2;
-    } else if (userArgs.indexOf("x") >= 0) {
+    } else if (originalProblem.indexOf("x") >= 0) {
       answer = value1 * value2;
-    } else if (userArgs.indexOf("/") >= 0) {
+    } else if (originalProblem.indexOf("/") >= 0) {
       answer = value1 / value2;
-    } else if (userArgs.indexOf("%") >= 0) {
+    } else if (originalProblem.indexOf("%") >= 0) {
       answer = value1 % value2;
-    } else if (userArgs.indexOf("p") >=0) {
+    } else if (originalProblem.indexOf("p") >=0) {
       answer = Math.pow(value1, value2);
-    } else if (userArgs.indexOf("s") >= 0) {
+    } else if (originalProblem.indexOf("s") >= 0) {
         answer = Math.sqrt(value1);
-    } else if (userArgs.indexOf("+") == 0) {
-      answer = storedValue + value1;
     } else {answer = "I don't know how to do that!";}
  }
 
  console.log(answer);
 
-
-
+};
